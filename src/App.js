bf66093bb5d3958bee6627dba6222991ce566f9a
rajon <mostofarajon111@gmail.com>
2022-04-29 00:02:11 +0600

    footer and navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './pages/Shared/Footer/Footer';
+import Header from './pages/Shared/Header/Header';
 
 function App() {
   return (
     <div>
-      
+      <Header></Header>
+      <Routes>
+        <Route></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,10 +1,87 @@
 import React from 'react';
 
 const Footer = () => {
+    const year = new Date().getFullYear()
     return (
-        <div>
-            
-        </div>
+        <>
+        <footer className='mt-2'>
+            <div className='container'>
+                <div className="row">
+                    <div className="col-12 col-lg-10 mx-auto">
+                        <div className="row">
+                            <div className="col-6 col-lg-3">
+                                <h2 className='footer-sce-title'>Company</h2>
+                                <ul>
+                                    <li>
+                                        <a href="#">about</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">about</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">about</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">about</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="col-6 col-lg-3">
+                                <h2>Support</h2>
+                                <ul>
+                                    <li>
+                                        <a href="#">about</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">about</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">about</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">about</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="col-6 col-lg-3">
+                                <h2>Services</h2>
+                                <ul>
+                                    <li>
+                                        <a href="#">about</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">about</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className='text-white'>about</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">about</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="col-6 col-lg-3">
+                                <h2>Follow Us</h2>
+                                <div className="row">
+                                    <div className="col-3 mx-auto">
+                                        <a href="">
+                                         
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+                <hr />
+                <div className="mt-2 mb-0">
+                    <p className='text-center w-100'>copyright&copy; {year} . All right and reserved by Sports warehouse .</p>
+                </div>
+
+            </div>
+        </footer>
+        </>
     );
 };
 
