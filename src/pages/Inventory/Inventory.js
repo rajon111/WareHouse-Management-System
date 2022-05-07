0ac486487dbaa3d9b8aa54ef8fadef73dde9355f
rajon <mostofarajon111@gmail.com>
2022-05-07 19:39:14 +0600

    inventory update and restock done

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -1,19 +1,80 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useForm } from "react-hook-form";
+import useItem from '../Hooks/useItem';
+import { toast } from 'react-toastify';
 
 const Inventory = () => {
     const { id } = useParams()
     const { register, handleSubmit } = useForm();
     const onSubmit = data => console.log(data);
-    
+    const [product, setProduct] = useItem(id)
+
+    const handleDelivered = event =>{
+        const total = parseInt(product.quantity)
+        const manage = total - 1
+        const quantity = {manage}
+
+        const url = `http://localhost:5000/inventory/${id}`
+        fetch(url, {
+            method: 'put',//update or add
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(quantity)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log('success', data)
+            toast('Delivered')
+
+        })
+    }
+
+    const handleRestock = event =>{
+        event.preventDefault()
+        const quan = parseInt(product.quantity)
+        const total = parseInt(event.target.q.value)
+        const manage = quan + total
+        const quantity = {manage}
+
+        const url = `http://localhost:5000/inventory/${id}`
+        fetch(url, {
+            method: 'put',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(quantity)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log('success', data)
+            toast('Quantity Updated')
+            event.target.reset()
+        })
+
+    }
+
     return (
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input {...register("firstName", { required: true, maxLength: 20 })} />
-                <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-                <input type="number" {...register("age", { min: 18, max: 99 })} />
-                <input type="submit" />
+        <div className='md:w-1/2 mx-auto my-2 sm:w-full'>
+            <form className='flex flex-col mb-4 px-3' onSubmit={handleSubmit(onSubmit)}>
+                <h2 className='text-center text-2xl m-2'>Inventory</h2>
+                <div><img style={{ height: '300px ', width: '100%' }} src={product.picture} alt="" /></div>
+                <input placeholder='ID' value={id} className='border p-2 mb-2 ' {...register("id", { required: true })} />
+                <input placeholder='Name' value={product.name} className='border p-2 mb-2 ' {...register("name", { required: true })} /> 
+                <input placeholder='Description' value={product.description} className='border p-2 mb-2' {...register("description")} />
+                <input placeholder='Supplier' value={product.supplier} className='border p-2 mb-2'{...register("supplier")} />
+                <input placeholder='Price' value={product.price} className='border p-2 mb-2'  {...register("price")} />
+                <input placeholder='Quantity' value={product.quantity} className='border p-2 mb-2' type="number" {...register("quantity")} />
+                <input className='border p-2 mb-2 btn bg-primary text-white' onClick={handleDelivered} type="submit" value='Deliverd' />
+                
+            </form>
+
+            <form onSubmit={handleRestock}>
+                <div className='text-center'>
+                    <input className='border pl-40 pt-2 pb-2 mb-2 d-block mx-auto w-50' type='number' name='q' placeholder='Quantity' />
+                    <input className='border p-2 btn bg-primary text-white d-block mx-auto w-50' type="submit" value="Restock" />
+                </div>
             </form>
             
         </div>
